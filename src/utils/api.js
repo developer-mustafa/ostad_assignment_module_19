3242fd32406f2fb270515940e2e74c1acb4fdc71
developer-mustafa <mustafarahman792@gmail.com>
2024-04-25 09:17:55 +0600

    test1

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:9100/api",
+  baseURL: "https://food-crud-1-server.vercel.app/api",
 });
 
 export default api;
